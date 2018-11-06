2a92eaf7d05a4ef1872b74b869a15d17ff8f003e
lprice93 <lprice@wilbron.com>
2018-11-05 18:31:35 -0600

    resume lab

---
+++ resume.js
@@ -0,0 +1,16 @@
+let resume = {
+    fullName: 'Lindsay Price',
+    jobTitle: 'Art Director',
+    description: 'I\'m an artist from Alabama and a graduate from the University of Montevallo where I acquired a degree in Graphic Design and a minor in Deaf Studies.',
+    interests: 'Working out, Painting, Watching Movies, Hanging with friends',
+    pastPositions: 'Legacy Gymnastics: Coach, Wilbron Inc: Graphic Design Intern, Wilbron Inc: Art Director',
+    skills: 'Adobe Creative Cloud,'
+}
+console.log(resume);
+resume.fullName.replace = uppercase;
+/* Your name
+Your career/field
+A short description of yourself
+A list of your interests
+A list of your past positions that includes company/place name, title, short description of what you did
+A list of your skills */
