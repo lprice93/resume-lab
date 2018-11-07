7902fcbc487a371a0ba6c6898dc6bf4d4c9be994
lprice93 <43084188+lprice93@users.noreply.github.com>
2018-11-06 20:09:25 -0600

    finish console.logs

--- resume.js
+++ resume.js
@@ -2,12 +2,33 @@ let resume = {
     fullName: 'Lindsay Price',
     jobTitle: 'Art Director',
     description: 'I\'m an artist from Alabama and a graduate from the University of Montevallo where I acquired a degree in Graphic Design and a minor in Deaf Studies.',
-    interests: 'Working out, Painting, Watching Movies, Hanging with friends',
-    pastPositions: 'Legacy Gymnastics: Coach, Wilbron Inc: Graphic Design Intern, Wilbron Inc: Art Director',
-    skills: 'Adobe Creative Cloud,'
+    interests: "Working out \n Painting \n Watching Movies \n Hanging with friends\n",
+    pastPositions: "Legacy Gymnastics: Coach \n , Wilbron Inc: Graphic Design Intern \n , Wilbron Inc: Art Director" ,
+    skills: [
+        "Adobe Creative Cloud",
+        "Painting",
+        "Sleeping",
+        "American Sign Language" ]
+};
+
+console.log("Name:\n " + resume.fullName.toUpperCase()); //string concatenation. \ = escape charactrer \n = new line
+console.log(resume.jobTitle);
+console.log(description);
+console.log(resume.interests);
+console.log(pastPositions)
+console.log(resume.skills[0]);
+
+function beCoolSkills(isCool, skill) {
+    if (isCool === true) {
+        console.log('BAM ' + skill);
+    } else {
+        console.log(skill);
+    }
 }
-console.log(resume);
-resume.fullName.replace = uppercase;
+
+beCoolSkills(false, "Adobe Creative Cloud");
+beCoolSkills(true, "Painting");
+
 /* Your name
 Your career/field
 A short description of yourself
